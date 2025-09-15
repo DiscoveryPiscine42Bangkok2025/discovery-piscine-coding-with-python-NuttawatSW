fcbe8c805eaa7590e84ad67f69aa2ab738c7543f
NuttawatSW <66050229@kmitl.ac.th>
2025-09-15 19:41:26 +0700

    Update isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 a = int(input())
 if(a==0): 
     print("This number is both positive and negative.")
 elif(a>0): 
     print("This number is positive.")
 elif(a<0): 
+
     print("This number is negative.")
