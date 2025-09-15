4bcf2dbef2e6f75e45ad19d5387cc902c2c1a1ab
NuttawatSW <66050229@kmitl.ac.th>
2025-09-16 00:30:17 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+a = int(input("กรอกเลข"))
+if(a==0): 
+    print("This number is both positive and negative.")
+elif(a>0): 
+    print("This number is positive.")
+elif(a<0): 
+    print("This number is negative.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+password = "Python is awesome"
+in_putpassword = input("Enter your password: ")
+
+if(in_putpassword==password):print("ACCESS GRANTED")
+else:print("ACCESS DENIED")
