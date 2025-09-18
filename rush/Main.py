bd68b919f163a0d04a680ed1b6b712aa178f874b
NuttawatSW <66050229@kmitl.ac.th>
2025-09-18 22:32:16 +0700

    Add files via upload

---
+++ rush/Main.py
@@ -0,0 +1,27 @@
+from Checkmate import Checkmate
+
+def main():
+    board = """\
+            ..
+            .K
+            \
+            """
+    Checkmate(board)
+
+    board = """\
+            ......
+            .K....
+            P..P..
+            ......
+            ......
+            ......
+            \
+            """
+    Checkmate(board)
+
+
+
+
+
+main()
+
